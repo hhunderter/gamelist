11ba1cc536b429478899fac64f410bd7bbb28e5a
blackcoder87 <blackcoder@gmx.de>
2019-02-06 14:00:11 +0100

    Invalid argument supplied for foreach()

--- upload/application/modules/gamelist/views/admin/cats/index.php
+++ upload/application/modules/gamelist/views/admin/cats/index.php
@@ -1,7 +1,7 @@
 <?php $gamesMapper = $this->get('gamesMapper'); ?>
 
 <h1><?=$this->getTrans('menuCats') ?></h1>
-<?php if ($this->get('cats') != ''): ?>
+<?php if (!empty($this->get('cats'))): ?>
     <form class="form-horizontal" method="POST" action="">
         <?=$this->getTokenField() ?>
         <div class="table-responsive">
